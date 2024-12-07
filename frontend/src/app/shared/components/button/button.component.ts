e300d6ce9f535509a8286f2282a7b26ee9a0bc92
MateuszJankowski02 <twonightdragons@gmail.com>
2024-12-07 20:25:45 +0100

    Adjusted button and began working on header

--- frontend/src/app/shared/components/button/button.component.ts
+++ frontend/src/app/shared/components/button/button.component.ts
@@ -1,11 +1,14 @@
-import { Component, input, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
   title = input<string>('');
+  color = input<'button-border-1' |
+                'button-border-2'>('button-border-1');
 }
