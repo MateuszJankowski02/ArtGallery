94f94065c71add7d21377a2c6520a6b25d2730bb
MateuszJankowski02 <twonightdragons@gmail.com>
2024-12-08 01:02:14 +0100

    Added logo to header component; Added new favicon

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../button/button.component";
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, NgOptimizedImage],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
