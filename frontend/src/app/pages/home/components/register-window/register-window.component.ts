9b5d8717f6effd3cddcdab3c3d2ee4297a13089b
MateuszJankowski02 <twonightdragons@gmail.com>
2025-01-05 19:33:30 +0100

    Created register-window component

---
+++ frontend/src/app/pages/home/components/register-window/register-window.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-window',
+  imports: [],
+  templateUrl: './register-window.component.html',
+  styleUrl: './register-window.component.scss'
+})
+export class RegisterWindowComponent {
+
+}
