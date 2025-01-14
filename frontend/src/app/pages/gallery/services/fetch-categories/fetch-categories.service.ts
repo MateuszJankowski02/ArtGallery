84e629fa3e7facc52e441509c6ed1d6c10916501
MateuszJankowski02 <twonightdragons@gmail.com>
2025-01-14 18:06:05 +0100

    Implemented fetch-categories

--- frontend/src/app/pages/gallery/services/fetch-categories/fetch-categories.service.ts
+++ frontend/src/app/pages/gallery/services/fetch-categories/fetch-categories.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,29 @@ import { Injectable } from '@angular/core';
 export class FetchCategoriesService {
 
   constructor() { }
+  // fetch first 10 categories
+  async getCategories(): Promise<any> {
+    try {
+      const response = await axios.get('http://localhost:8000/api/categories/');
+      response.data = response.data.slice(0, 10);
+
+      if(response.status === 200){
+        console.log('Categories fetched successfully:', response.data);
+        console.log('Status:', response.status);
+      }
+
+      return response;
+
+    }catch (error: any) {
+      if (error.response) {
+        console.error('Validation Error:', error.response);
+        return error.response;
+      } else if (error.request) {
+        console.error('No response received:', error.request);
+      } else {
+        console.error('Error:', error.message);
+      }
+    }
+
+  }
 }
